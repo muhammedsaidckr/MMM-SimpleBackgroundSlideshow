817d314ced6f734514ef1a5b39d52397c6a01835
Muhammed Said Çakır <muhammedsaidckr@gmail.com>
2024-02-29 23:27:07 +0300

    Updated Readme file and package json corrupted

--- MMM-SimpleBackgroundSlideshow.js
+++ MMM-SimpleBackgroundSlideshow.js
@@ -1,59 +1,53 @@
 Module.register("MMM-SimpleBackgroundSlideshow", {
-	defaults: {
-		urls: []
-	},
+    defaults: {
+        urls: []
+    },
 
-	start () {
-		this.currentUrlIndex = 0;
-		this.config.identifier = this.identifier;
-		// console.log(this.urls);
-	},
+    start() {
+    },
 
-	getDom () {
-		let wrapper = document.createElement("div");
-		wrapper.id = "vegas-slideshow"; // Assign an ID to the wrapper
-		return wrapper;
-	},
+    getDom() {
+        let wrapper = document.createElement("div");
+        wrapper.id = "vegas-slideshow"; // Assign an ID to the wrapper
+        return wrapper;
+    },
 
-	getScripts() {
-		return [
-			"/modules/MMM-SimpleBackgroundSlideshow/node_modules/jquery/dist/jquery.min.js",
-			"vegas.js"
-		];
-	},
-	getStyles() {
-		return ["vegas.css", "MMM-SimpleBackgroundSlideshow.css"];
-	},
-	notificationReceived (notification, payload, sender) {
-		let urls = this.config.urls.map((url) => {
-			return { src: url };
-		});
-		if (notification === "DOM_OBJECTS_CREATED") {
-			console.log("DOM objects created");
-			// eslint-disable-next-line no-undef
-			$(document).ready(function() {
-				// eslint-disable-next-line no-undef
-				let $elmt = $("#vegas-slideshow");
-				// console.log($elmt);
-				// eslint-disable-next-line no-undef
-				console.log(urls);
-				$elmt.vegas({
-					delay: 7000,
-					timer: false,
-					shuffle: true,
-					firstTransition: "fade",
-					firstTransitionDuration: 5000,
-					transition: "random",
-					animation: "random",
-					transitionDuration: 5000,
-					cover: true,
-					color: "black",
-					align: "top",
-					valign: "center",
-					slides: urls
-				});
-			});
-		}
-	}
+    getScripts() {
+        return [
+            "/modules/MMM-SimpleBackgroundSlideshow/node_modules/jquery/dist/jquery.min.js",
+            "vegas.js"
+        ];
+    },
+    getStyles() {
+        return ["vegas.css", "MMM-SimpleBackgroundSlideshow.css"];
+    },
+    notificationReceived(notification, payload, sender) {
+        let urls = this.config.urls.map((url) => {
+            return {src: url};
+        });
+        if (notification === "DOM_OBJECTS_CREATED") {
+            // eslint-disable-next-line no-undef
+            $(document).ready(function () {
+                // eslint-disable-next-line no-undef
+                let $elmt = $("#vegas-slideshow");
+                // eslint-disable-next-line no-undef
+                $elmt.vegas({
+                    delay: 7000,
+                    timer: false,
+                    shuffle: true,
+                    firstTransition: "fade",
+                    firstTransitionDuration: 5000,
+                    transition: "random",
+                    animation: "random",
+                    transitionDuration: 5000,
+                    cover: true,
+                    color: "black",
+                    align: "top",
+                    valign: "center",
+                    slides: urls
+                });
+            });
+        }
+    }
 
 });
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # MMM-SimpleBackgroundSlideshow
+MMM-SimpleBackgroundSlideshow is a module for MagicMirror² that allows you to display a simple, customizable background slideshow on your MagicMirror². This module is designed to be lightweight and easy to configure, making it the perfect addition to any MagicMirror² setup.
+
+## Installation
+1. Navigate to your MagicMirror's modules directory and clone this repository:
+    ```bash
+    git clone https://github.com/muhammedsaidckr/MMM-SimpleBackgroundSlideshow.git
+    ```
+2. Configure the module in your config.js file.
+
+## Usage
+
+```js
+modules: [
+    {
+        module: "MMM-SimpleBackgroundSlideshow",
+        position: "fullscreen_below", // This can be any of the regions that support background content.
+        config: {
+            // See 'Configuration options' for more information.
+        }
+    }
+]
+```
+
+### Configuration Options
+
+`urls` => Array: List of image URLs to be displayed in the slideshow.
+	
+#### Contributing
+
+Contributions are welcome! Please read the contributing guidelines before getting started.
+
+#### License
+MMM-SimpleBackgroundSlideshow is licensed under the MIT License.
+
+
